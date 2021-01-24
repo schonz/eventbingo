85f433b6933f3352fc1d5ff3ffb2cbfdcc11bad7
Schonz <5674067+schonz@users.noreply.github.com>
2021-01-23 22:17:46 -0600

    App.js: rearrange components on screen

--- frontend/eventbingo/src/App.js
+++ frontend/eventbingo/src/App.js
@@ -8,8 +8,8 @@ import BingoRules from './pages/BingoRules';
 function App() {
   return (
     <div className="App">
-      <BingoRules URL_BASE={config.url.API_URL} style={{padding: "100px"}}/>
       <BingoBoard URL_BASE={config.url.API_URL}/>
+      <BingoRules URL_BASE={config.url.API_URL} style={{padding: "100px"}}/>
     </div>
   );
 }
