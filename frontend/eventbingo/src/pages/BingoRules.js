d449968b74d6a79df58c31f3a27741bbd7e524fd
Schonz <5674067+schonz@users.noreply.github.com>
2021-05-10 23:09:42 -0500

    BingoRules.js: Added ability to delete rule in UI

--- frontend/eventbingo/src/pages/BingoRules.js
+++ frontend/eventbingo/src/pages/BingoRules.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
-import TextField from '@material-ui/core/TextField';
+import Input from '@material-ui/core/Input';
+import FormControl from '@material-ui/core/FormControl';
 
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -10,6 +11,8 @@ import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+
+import DeleteIcon from '@material-ui/icons/Delete';
 
 class BingoRules extends React.Component{
 constructor(props){
@@ -40,6 +43,18 @@ constructor(props){
         this.req_AllRules();
     }
 
+    async req_DelRule(data){
+        var url = this.URL_BASE + "/api/remove_rule";
+
+        await(fetch(url, {
+            method: 'DELETE',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(data)
+        }))
+
+        this.req_AllRules();
+    }
+
     req_AllRules(){
         fetch(this.URL_BASE + "/api/all_rules")
             .then(response => response.json())
@@ -47,7 +62,7 @@ constructor(props){
     }
 
     set_AllRules(data){
-        this.setState({rules: data});
+        this.setState({rules: data.reverse()});
     }
 
     addRule(){
@@ -66,9 +81,21 @@ constructor(props){
         data = JSON.parse(strJson);
 
         this.req_AddRule(data);
+        this.setState({newRuleText: ""})
     }
 
-    _handle_ruleText_TextFieldChange(e){
+    delRule(rule){
+        var data;
+        var strJson;
+
+        console.log("deleting: " + rule.id);
+        strJson = '{ "id" : "' + rule.id + '"}';
+        data = JSON.parse(strJson);
+
+        this.req_DelRule(data);
+    }
+
+    _handle_ruleText_InputChange(e){
         var txt;
         txt = e.target.value
         this.setState({newRuleText: txt});
@@ -77,31 +104,45 @@ constructor(props){
     render() {
         return <div>
             <Paper style={{display: "flex"}}>
-                <Button style={{margin: "10px"}} onClick={() => this.addRule()}>
-                    Add Rule
-                </Button>
-                <TextField
-                id="rule-text"
-                label="Enter rule here"
-                variant="filled"
-                margin="normal"
-                value={this.state.newRuleText}
-                onChange={(e) => this._handle_ruleText_TextFieldChange(e)}/>
+
+                    <Input
+                    id="rule-text"
+                    label="Enter rule here"
+                    variant="filled"
+                    margin="normal"
+                    value={this.state.newRuleText}
+                    onChange={(e) => this._handle_ruleText_InputChange(e)}/>
+                    <Button type="" 
+                    variant="contained" 
+                    color="primary" 
+                    style={{margin: "10px"}} 
+                    ClickMode="Press"
+                    onClick={() => this.addRule()}>
+                        Add Rule
+                    </Button>
+
             </Paper>
             <TableContainer component={Paper}>
                 <Table className="table-rules" aria-label="simple table">
                     <TableHead>
                         <TableRow>
-                            <TableCell>Rule</TableCell>
+                            <TableCell>Rules</TableCell>
+                            <TableCell></TableCell>
+                            <TableCell></TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
                         {this.state.rules.map((row) => (
                             <TableRow key={row.id}>
-                                <TableCell component="th" scope="row">
+                                <TableCell component="th" scope="row" width="5">
                                     {row.id}
                                 </TableCell>
-                        <TableCell align="left">{row.text}</TableCell>
+                                <TableCell component="th" scope="row" width="10">
+                                    <DeleteIcon onClick={() => this.delRule(row)}/>
+                                </TableCell>
+                                <TableCell component="th" scope="row" align="left">
+                                    {row.text}
+                                </TableCell>
                             </TableRow>
                         ))}
                     </TableBody>
