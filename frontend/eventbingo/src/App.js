3ccb5fbaa4541d5fe0702b0a62d7e2810d6face9
Schonz <5674067+schonz@users.noreply.github.com>
2021-05-11 21:58:58 -0500

    App.js: Style update

--- frontend/eventbingo/src/App.js
+++ frontend/eventbingo/src/App.js
@@ -7,9 +7,9 @@ import BingoRules from './pages/BingoRules';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{padding: "10px"}}>
       <BingoBoard URL_BASE={config.url.API_URL}/>
-      <BingoRules URL_BASE={config.url.API_URL} style={{padding: "100px"}}/>
+      <BingoRules URL_BASE={config.url.API_URL}/>
     </div>
   );
 }
